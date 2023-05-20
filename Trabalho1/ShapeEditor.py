c6ce72a5e41791151832c9d55a067fc93cecd8ee
BarbaraCerqueira <1barbararodrigues@gmail.com>
2023-05-20 14:07:31 -0300

    Adicionada função de escala

--- Trabalho1/ShapeEditor.py
+++ Trabalho1/ShapeEditor.py
@@ -79,7 +79,7 @@ class Circle(object):
         glPopMatrix()
 
 picked = None
-modeConstants = ["CREATE RECT", "CREATE CIRCLE", "TRANSLATE", "ROTATE"]
+modeConstants = ["CREATE RECT", "CREATE CIRCLE", "TRANSLATE", "ROTATE", "SCALE"]
 mode = modeConstants[0]
 
 def reshape( width, height):
@@ -90,7 +90,7 @@ def reshape( width, height):
     glMatrixMode (GL_MODELVIEW)
 
 def mouse (button, state, x, y): # Ação após primeiro clique
-    global lastx,lasty,picked
+    global lastx,lasty,picked,firstx,firsty
     if state!=GLUT_DOWN: return
     if mode == "CREATE RECT":
         shapes.append(Rect([[x,y],[x,y]]))
@@ -99,16 +99,25 @@ def mouse (button, state, x, y): # Ação após primeiro clique
     elif mode == "TRANSLATE":
         picked = None
         for s in shapes:
-            if s.contains([x,y]): picked = s
+            if s.contains([x,y]): 
+                picked = s
         lastx,lasty = x,y 
     elif mode == "ROTATE":
         picked = None
         for s in shapes:
-            if s.contains([x,y]): picked = s
+            if s.contains([x,y]): 
+                picked = s
         lastx,lasty = x,y 
+    elif mode == "SCALE":
+        picked = None
+        for s in shapes:
+            if s.contains([x, y]):
+                picked = s
+        lastx,lasty = x,y
+        firstx,firsty = x,y
 
 def mouse_drag(x, y): # Ação durante arrastar do mouse
-    global lastx,lasty
+    global lastx,lasty,firstx,firsty
     if mode == "CREATE RECT":
         shapes[-1].set_point(1,[x,y])
     elif mode == "CREATE CIRCLE":
@@ -123,20 +132,51 @@ def mouse_drag(x, y): # Ação durante arrastar do mouse
             lastx, lasty = x, y
     elif mode == "ROTATE":
         if picked:
+            center = picked.get_center()
+
             # Vetores entre o ponto clicado e o centro da forma
-            last_vector = [lastx - picked.get_center()[0], lasty - picked.get_center()[1]]
-            current_vector = [x - picked.get_center()[0], y - picked.get_center()[1]]
+            last_vector = [lastx - center[0], lasty - center[1]]
+            current_vector = [x - center[0], y - center[1]]
 
             angle = (math.atan2(current_vector[1], current_vector[0]) 
                      - math.atan2(last_vector[1], last_vector[0]))
 
             # Traz a forma para o centro do eixo, rotaciona e a leva de volta
-            t = create_from_translation([-picked.get_center()[0], -picked.get_center()[1], 0])
+            t = create_from_translation([-center[0], -center[1], 0])
             rotation_matrix = create_from_eulers([0, -angle, 0])
-            rt = create_from_translation([picked.get_center()[0], picked.get_center()[1], 0])
+            rt = create_from_translation([center[0], center[1], 0])
             transform_matrix = t @ rotation_matrix @ rt
-            picked.set_matrix(picked.m @ transform_matrix)
-            lastx, lasty = x, y           
+
+            picked.set_matrix(picked.m @ transform_matrix) # Aplica transformação
+            lastx, lasty = x, y
+    elif mode == "SCALE":
+        if picked:
+            center = picked.get_center()
+            if center[0] != firstx or center[1] != firsty:
+                scale_axis = np.array([firstx - center[0], firsty - center[1]]) # Eixo de escala definido pelo primeiro clique
+                angle_to_x = -math.atan2(scale_axis[1], scale_axis[0]) # Angulo entre o eixo de escalonamento e o eixo X, padrão antihorario
+
+                # Vetores do centro da forma até os pontos de clique atual e anterior do mouse
+                mouse_axis = np.array([x - center[0], y - center[1]])
+                last_mouse_axis = np.array([lastx - center[0], lasty - center[1]])
+
+                # Projeção dos vetores do mouse sobre o eixo de escala
+                proj = np.dot(mouse_axis, scale_axis) / np.linalg.norm(scale_axis)
+                last_proj = np.dot(last_mouse_axis, scale_axis) / np.linalg.norm(scale_axis)
+
+                scale_factor = proj/last_proj
+
+                # Traz a forma para o centro do eixo, alinha o eixo de escala com o eixo X, escalona e retorna
+                t = create_from_translation([-center[0], -center[1], 0])
+                rot = create_from_eulers([0, -angle_to_x, 0])
+                scale_matrix = create_from_scale([scale_factor, 1, 1])
+                rrot = create_from_eulers([0, angle_to_x, 0])
+                rt = create_from_translation([center[0], center[1], 0])
+                transform_matrix = t @ rot @ scale_matrix @ rrot @ rt
+
+                picked.set_matrix(picked.m @ transform_matrix) # Aplica transformação
+                lastx, lasty = x, y
+       
     glutPostRedisplay()
 
 def display():
